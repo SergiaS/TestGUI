971503198bf8a902d788662fe04bb1cc1735b0f0
sergias <sergias@ukr.net>
2020-08-10 14:12:01 +0300

    Lesson 2: JavaBeans, Containers, Skins

--- src/main/java/TestGUI.java
+++ src/main/java/TestGUI.java
@@ -1,25 +1,43 @@
 package main.java;
 
-import main.java.hw._01.MyJButton;
-import main.java.hw._01.MyJFrame;
-import main.java.hw._01.MyJPanel;
-
+import javax.swing.*;
+import javax.swing.plaf.metal.MetalLookAndFeel;
 import java.awt.*;
 
 public class TestGUI {
 	public static void main(String[] args) {
-		MyJButton button1 = new MyJButton("Button 1");
-		MyJButton button2 = new MyJButton("Button 2");
-		MyJButton button3 = new MyJButton("Button 3");
-		MyJButton button4 = new MyJButton("Button 4");
+		try {
+//			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//			UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+			UIManager.setLookAndFeel(new MetalLookAndFeel()); // команда для изменения внешнего вида внутренних компонентов
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
 
-		MyJPanel panel1 = new MyJPanel("Panel", 100, 100, button1, button2, button3);
-		panel1.setBackground(Color.RED);
+		JFrame.setDefaultLookAndFeelDecorated(true); // команда для изменения внешнего вида самого окна
 
-		MyJPanel panel2 = new MyJPanel("Panel", 100, 100, button4);
-		panel2.setBackground(Color.BLUE);
+		JFrame frame = new JFrame("Test Frame");
+		frame.setIconImage(new ImageIcon("https://i-dgtl.ru/wp-content/uploads/2017/01/skype-icon-15.png").getImage());
+//		frame.setIconImage(new ImageIcon("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTY2kUXD6LqDNvdN93NiHhQLlvxAkd8KTtJ6Q&usqp=CAU").getImage());
 
-		MyJFrame frame = new MyJFrame("Frame", 400, 300, panel1, panel2);
+		frame.setSize(400, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+		frame.setLocationRelativeTo(null);
+
+		FlowLayout fl = new FlowLayout();
+		frame.setLayout(fl);
+
+		JButton button = new JButton("Test Button");
+		button.setSize(300, 200);
+
+		JPanel panel = new JPanel(fl);
+		panel.setBorder(BorderFactory.createEtchedBorder());
+		panel.setSize(300, 300);
+		panel.add(button);
+
+		frame.getContentPane().add(panel);
 
 	}
 }
